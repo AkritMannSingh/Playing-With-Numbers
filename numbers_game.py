1010d30d9843cecc578789c4d9635317e791d40f
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-10-31 05:33:29 +0530

    Create numbers_game.py

---
+++ numbers_game.py
@@ -0,0 +1,14 @@
+print("Enter three numbers to begin the game.")
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter the third number:"))
+
+if(a + b + c > 1000):
+   print("You win the game!")
+
+elif(a + b + c == 1000):
+   print("You make the game Foul!")
+
+else:
+  print("You lose the game!")
