9a50af993211dcca72b613980fb4db0aa5c2c0bd
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-10-31 05:36:43 +0530

    Create identification_numbers.py

---
+++ identification_numbers.py
@@ -0,0 +1,8 @@
+v = int(input("Enter a number:"))
+
+if(v > 0):
+  print("Number is positive")
+elif(v == 0):
+  print("Above number is neither positive nor negative.")
+else:
+  print("Number is negative")
