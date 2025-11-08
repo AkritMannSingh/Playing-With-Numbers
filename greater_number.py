c094951caede38584dd43ae583dcfa6b07966036
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-08 12:05:15 +0530

    Crear greater_number.py

---
+++ greater_number.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number"))
+
+if(a>b):
+  print("First number is greater the second number.")
+
+elif(a==b):
+  print("Both numbers are equal.")
+
+else:
+  print("Second number is graeter than first number.")
