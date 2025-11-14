6c204512ac4d7d46601818f29836a0039cffd238
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-14 06:58:38 +0530

    Create multiple_finder.py

---
+++ multiple_finder.py
@@ -0,0 +1,6 @@
+print("Hey! Welcome to Multiple Finder...\n")
+
+num = int(input("Enter a number:"))
+print("First 10 multiples of", num, "are:\n", num * 1, "\n", num * 2, "\n", num * 3, "\n", num * 4, "\n", num * 5, "\n", num * 6, "\n", num * 7, "\n", num * 8, "\n", num * 9, "\n", num * 10, "\n")
+
+str(input("How was your experience with Multiple Funder?\n"))
