9cf3c83bb64d26191b146f82b86df081d956fb81
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-15 06:59:37 +0530

    Create average_number.py

---
+++ average_number.py
@@ -0,0 +1,12 @@
+import random
+
+num1 = random.randint(450, 950)-450
+num2 = random.randint(450, 950)-450
+
+avg = (num1 + num2)
+
+print("Random integers in the \ range 450 to 950:", num1, num2)
+
+print("Their Average:", avg)
+
+print("Thank You for Using it")
