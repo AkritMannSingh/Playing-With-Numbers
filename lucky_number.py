0d478354e7b4765e1fcdabde6ebc7194ee803f67
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-27 06:35:57 +0530

    Create lucky_number.py

---
+++ lucky_number.py
@@ -0,0 +1,16 @@
+print("In this Interesting Section we will find our lucky number..\n")
+
+print(" ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\n")
+
+number = int(input('Enter a number:'))
+if(number > 0):
+
+  if(number % 2 == 0):
+  print("Lucky number")
+
+  else:
+    print("Not a lucky number")
+else:
+  print("Try again.....\n")
+
+print("♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪")
