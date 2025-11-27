3c74466a2ca184adc1d8736ab2acce86f47556d3
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-27 06:46:06 +0530

    Create greatest_number.py

---
+++ Numbers-Programming/greatest_number.py
@@ -0,0 +1,12 @@
+print("Welcome! I this program we check that whether number are greater than or equals to each other.\n")
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number"))
+
+if(a>b):
+  print("First number is greater the second number.")
+
+elif(a==b):
+  print("Both numbers are equal.")
+
+else:
+  print("Second number is graeter than first number.")
