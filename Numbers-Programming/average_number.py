2c5925ac716e157f9b39d00e9d678ffe1223b7ea
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-27 06:43:09 +0530

    Update average_number.py

--- Numbers-Programming/average_number.py
+++ Numbers-Programming/average_number.py
@@ -1,1 +1,14 @@
+import random
 
+print("We will find Average of Numbers🔢\n")
+
+num1 = random.randint(450, 950)-450
+num2 = random.randint(450, 950)-450
+
+avg = (num1 + num2)
+
+print("Random integers in the \ range 450 to 950:", num1, num2)
+
+print("Their Average:", avg)
+
+print("Thank You for Using it")
