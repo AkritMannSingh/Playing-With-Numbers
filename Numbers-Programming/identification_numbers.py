4d54cc0d087ae307d5a9d607512ff58f2ffa9680
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-30 06:45:51 +0530

    Create identification_numbers.py

---
+++ Numbers-Programming/identification_numbers.py
@@ -0,0 +1,14 @@
+print("☑️In this section we will check the number either positive or negative.\n")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+ 
+v = int(input("Enter a number:"))
+
+if(v > 0):
+  print("Number is positive")
+elif(v == 0):
+  print("Above number is neither positive nor negative.")
+else:
+  print("Number is negative\n")
+
+print("The End🔚")
