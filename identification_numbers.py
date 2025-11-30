1e6ed9c7a9cb4012e68f37275e5ea551be274917
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-30 06:47:01 +0530

    Delete identification_numbers.py

--- identification_numbers.py
+++
@@ -1,8 +0,0 @@
-v = int(input("Enter a number:"))
-
-if(v > 0):
-  print("Number is positive")
-elif(v == 0):
-  print("Above number is neither positive nor negative.")
-else:
-  print("Number is negative")
